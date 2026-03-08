fdec7772889629fed51207dab5c152f6fbb8a554
Diego Paulino <diegopaulino.dev@gmail.com>
2026-03-08 17:57:12 -0300

    feat: integrar componentes e concluir montagem da Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,51 +1,39 @@
-import { useEffect, useState } from "react";
-import { Button } from "../../components/Button/Button";
+import { useState } from "react";
 
-import { Carousel, CarouselItem } from "../../components/Carousel/Carousel";
-
-const getData = async () => {
-  try {
-    const response = await fetch(
-      "https://burgerlivery-esposito-api.onrender.com/offer-gallery",
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching offers:", error);
-  }
-};
-
-type Offer = {
-  id: number;
-  title: string;
-  description: string;
-  image: {
-    src: string;
-    alt: string;
-  };
-};
+import { Navigation } from "../../components/Navigation/Navigation";
+import Header from "../../components/Header/Header";
+import Offers from "../../components/Offers/Offers";
+import OrderAgain from "../../components/OrderAgain/OrderAgain";
+import MostWanted from "../../components/MostWanted/MostWanted";
+import Footer from "../../components/Footer/Footer";
+import AddressDialog from "../../components/AddressDialog/AddressDialog";
 
 function Home() {
-  const [offers, setOffers] = useState<Array<Offer>>([]);
+  /* O estado do dialog fica na página para permitir que o Header dispare a abertura
+     e o AddressDialog controle o fechamento sem acoplamento direto entre os componentes. */
+  const [isAddressDialogOpen, setIsAddressDialogOpen] = useState(false);
 
-  useEffect(() => {
-    getData().then((data) => {
-      setOffers(data);
-    });
-  }, []);
+  function handleOpenAddressDialog() {
+    setIsAddressDialogOpen(true);
+  }
+
+  function handleCloseAddressDialog() {
+    setIsAddressDialogOpen(false);
+  }
 
   return (
     <>
-      <Carousel>
-        {offers.map((offer) => (
-          <CarouselItem
-            key={offer.id}
-            title={offer.title}
-            description={offer.description}
-            image={offer.image}
-          />
-        ))}
-      </Carousel>
+      <Navigation />
+      <Header onOpenAddressDialog={handleOpenAddressDialog} />
+      <Offers />
+      <OrderAgain />
+      <MostWanted />
+      <Footer />
+
+      <AddressDialog
+        isOpen={isAddressDialogOpen}
+        onClose={handleCloseAddressDialog}
+      />
     </>
   );
 }
