1b808577a8306c2a02753e5284e740f0a2906266
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2026-04-16 18:00:40 -0300

    feat: add service request from payment methods

---
+++ src/api/postGeneratePix.ts
@@ -0,0 +1,32 @@
+import type { PixParams } from "../types";
+
+async function postGeneratePix(payload: PixParams) {
+  const url = import.meta.env.VITE_API_URL;
+  const token = JSON.parse(sessionStorage.getItem("userToken") as string);
+
+  const response = await fetch(`${url}/payment-methods/pix`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+
+    const errorMessage = {
+      error: errorData.error,
+      statusCode: response.status,
+      message: errorData.message,
+    };
+
+    throw new Error(JSON.stringify(errorMessage));
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+export default postGeneratePix;
