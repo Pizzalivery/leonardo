204050e182141a6760cbd5915fdb8d3ebf4adcde
Luana Oliveira <luana.2327891@gmail.com>
2026-04-21 13:50:02 -0300

    alterando deliveryadress para ficar nulo quando nao cadastrado

--- src/components/DeliveryAddress/DeliveryAddress.tsx
+++ src/components/DeliveryAddress/DeliveryAddress.tsx
@@ -1,12 +1,34 @@
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, MapPin } from "lucide-react";
 import "./DeliveryAddress.css";
+import { useNavigate } from "react-router";
 
 interface DeliveryAddressProps {
   address: string;
   onClick: () => void;
 }
 
+
+
+
+
 export const DeliveryAddress = ({ address, onClick }: DeliveryAddressProps) => {
+  const navigate = useNavigate();
+if (!address) {
+  return (
+    <div className="delivery-address" style={{ display: "flex", justifyContent: "center" }}>
+      <button
+        id="change-address"
+        onClick={() => navigate("/register/cep")}
+        style={{ display: "flex", alignItems: "center", gap: "4px", fontSize: "12px" }}>
+        <MapPin size={12} />
+        <span style={{ display: "inline" }}>Adicionar endereço</span>
+      </button>
+    </div>
+  );
+}
+
+
+
   return (
     <div className="delivery-address">
       <span className="delivery-text">
