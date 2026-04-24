7158a85d5f8da7a40307924d664e7f1634708fcc
Renan <renan6551@gmail.com>
2026-04-24 20:29:32 -0300

    Feat: integra envio de CPF com API e navegação para telefone

---
+++ src/pages/Auth/AddCpf/AddCpf.tsx
@@ -0,0 +1,78 @@
+import { useState, useEffect, type ChangeEvent } from "react";
+import { useNavigate, useOutletContext } from "react-router";
+import { Button, Heading, Input } from "../../../components";
+import { type AuthLayoutContext } from "../../../components/Layouts/AuthLayout/AuthLayout";
+import postAuthCpf from "../../../api/postAuthCpf";
+
+function AddCpf() {
+  const navigate = useNavigate();
+  const { setTitle, setNavigationHistory } = useOutletContext<AuthLayoutContext>();
+
+  const [cpf, setCpf] = useState("");
+
+  useEffect(() => {
+    setTitle("Adicionar CPF");
+    setNavigationHistory("/auth/register");
+  }, [setTitle, setNavigationHistory]);
+
+  const handleCpfChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCpf(e.target.value);
+  };
+
+  const handleContinue = async () => {
+    try {
+      const token = sessionStorage.getItem("token") || "";
+
+      await postAuthCpf(
+        { cpf },
+        token
+      );
+
+      navigate("/auth/add-phone");
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao enviar CPF");
+    }
+  };
+
+  const handleSkip = () => {
+    navigate("/");
+  };
+
+  return (
+    <section className="grid grid-rows-[1fr_auto] gap-4 h-[calc(100vh-88px)]">
+      <div className="flex flex-col gap-6 items-center justify-center">
+        <div className="flex flex-col gap-2 text-center">
+          <Heading component="h1">Parabéns! Sua conta foi criada</Heading>
+          <p className="text-typography-base text-sm">
+            Finalize seu cadastro adicionando mais informações.
+          </p>
+        </div>
+
+        <Input
+          type="text"
+          name="cpf"
+          id="cpf"
+          label="CPF"
+          placeholder="Digite seu CPF *"
+          onChange={handleCpfChange}
+          fullWidth
+          noLabel
+        />
+
+        <Button variant="primary" onClick={handleContinue} fullWidth>
+          Continuar
+        </Button>
+
+        <button
+          onClick={handleSkip}
+          className="text-typography-base font-black text-base cursor-pointer"
+        >
+          Pular
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default AddCpf;
