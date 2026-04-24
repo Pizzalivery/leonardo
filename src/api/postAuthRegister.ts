4c2833a32e00976f49e34473833a77158cbb8983
Renan <renan6551@gmail.com>
2026-04-24 20:07:23 -0300

    Feat: adiciona requisição de CPF com autentificação

---
+++ src/api/postAuthRegister.ts
@@ -0,0 +1,45 @@
+export type RegisterPayload = {
+  name: string;
+  email: string;
+  password: string;
+  cpf?: string;
+  phone?: string;
+  role: string;
+  address?: {
+    cep: string;
+    street: string;
+    number: string;
+    neighborhood: string;
+    city: string;
+    state: string;
+  };
+};
+
+async function postAuthRegister(payload: RegisterPayload) {
+  const url = "https://burgerlivery-esposito-api.onrender.com";
+
+  const response = await fetch(`${url}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+
+    throw new Error(
+      JSON.stringify({
+        error: errorData.error,
+        statusCode: response.status,
+        message: errorData.message,
+      })
+    );
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+export default postAuthRegister;
