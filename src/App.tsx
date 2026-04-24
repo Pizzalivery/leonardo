b60dc7173621462bf35e3ebed92c00055f08061d
Pedro Goncalves <pedrogoncalvesr89@gmail.com>
2026-04-24 18:24:05 -0300

    Configura rotas do cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 
+import { PrivateRoutes } from "./components";
+import AuthLayout from "./components/Layouts/AuthLayout/AuthLayout";
 import NavigationLayout from "./components/Layouts/NavigationLayout/NavigationLayout";
 import ProductLayout from "./components/Layouts/ProductLayout/ProductLayout";
-import AuthLayout from "./components/Layouts/AuthLayout/AuthLayout";
 import "./styles/style.css";
-import { PrivateRoutes } from "./components";
 
 const Home = lazy(() => import("./pages/Home/Home"));
+const Login = lazy(() => import("./pages/Auth/Login/Login"));
+
+const Register = lazy(() => import("./pages/Register/Register/Register"));
+const RegisterCpf = lazy(() => import("./pages/Register/RegisterCpf/RegisterCpf"));
+const RegisterPhone = lazy(() => import("./pages/Register/RegisterPhone/RegisterPhone"));
+const RegisterCep = lazy(() => import("./pages/Register/RegisterCep/RegisterCep"));
+const RegisterAddress = lazy(() => import("./pages/Register/RegisterAddress/RegisterAddress"));
+
 const Search = lazy(() => import("./pages/Search/Search"));
 const Menu = lazy(() => import("./pages/Menu/Menu"));
+const ProductDetails = lazy(() => import("./pages/ProductDetails/ProductDetails"));
+
 const Orders = lazy(() => import("./pages/Orders/Orders"));
 const OrderDetails = lazy(() => import("./pages/OrderDetails/OrderDetails"));
-const ProductDetails = lazy(
-  () => import("./pages/ProductDetails/ProductDetails"),
-);
 const Profile = lazy(() => import("./pages/Profile/Profile"));
-const Login = lazy(() => import("./pages/Auth/Login/Login"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Carregando...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
+
+          <Route path="auth/login" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+          </Route>
+
+          <Route path="register" element={<AuthLayout />}>
+            <Route index element={<Register />} />
+            <Route path="cpf" element={<RegisterCpf />} />
+            <Route path="phone" element={<RegisterPhone />} />
+            <Route path="cep" element={<RegisterCep />} />
+            <Route path="address" element={<RegisterAddress />} />
+          </Route>
+
           <Route path="search" element={<NavigationLayout />}>
             <Route index element={<Search />} />
           </Route>
+
           <Route path="menu" element={<NavigationLayout />}>
             <Route index element={<Menu />} />
           </Route>
@@ -34,9 +54,7 @@ function App() {
           <Route path="products" element={<ProductLayout />}>
             <Route path=":productId" element={<ProductDetails />} />
           </Route>
-          <Route path="auth/login" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-          </Route>
+
           <Route element={<PrivateRoutes />}>
             <Route path="orders" element={<NavigationLayout />}>
               <Route index element={<Orders />} />
