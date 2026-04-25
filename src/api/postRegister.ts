483f06902ffbfbfdb7460297391f028c90e60a1d
Isabely Marques <isabelymarques844@gmail.com>
2026-04-24 21:45:58 -0300

    add registro

---
+++ src/api/postRegister.ts
@@ -0,0 +1,32 @@
+export type RegisterPayload = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+async function postRegister(payload: RegisterPayload) {
+  const url = "https://burgerlivery-esposito-api.onrender.com";
+
+  const response = await fetch(`${url}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    const errorMessage = {
+      error: errorData.error,
+      statusCode: response.status,
+      message: errorData.message,
+    };
+    throw new Error(JSON.stringify(errorMessage));
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+export default postRegister;
